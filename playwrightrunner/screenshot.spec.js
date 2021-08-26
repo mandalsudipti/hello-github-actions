8ccf093c20676abad80eaa46167e1172d08a7aad
sudipti <sudipti2017@gmail.com>
2021-08-26 14:32:47 +0530

    updating main.yml

--- playwrightrunner/screenshot.spec.js
+++
@@ -1,7 +0,0 @@
-const { test , expect} = require('@playwright/test') ;
-
-test('screenshot test', async ({ page }) => {
-    
-    await page.goto("https://en.wikipedia.org/wiki/Main_Page");
-    expect(await page.screenshot()).toMatchSnapshot('screenshot.png') ;
-  });
